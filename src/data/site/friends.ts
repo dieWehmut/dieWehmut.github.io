e41eaea86760822049e1e49891fafe42cfed3807
dieWehmut <diesehnsucht0@gmail.com>
2026-05-28 22:27:43 +0800

    1

--- src/data/site/friends.ts
+++ src/data/site/friends.ts
@@ -3,9 +3,24 @@ import type { FriendLink } from '../../types/content'
 export const friends: FriendLink[] = [
   {
     id: '',
+    name: 'Linca',
+    description: 'Zero Calorie Drink Shop',
+    url: 'https://lhcfl.github.io/',
+    avatar: 'https://github.com/lhcfl.png',
+  },
+  {
+    id: '',
     name: 'Je1ghtxyuN',
     description: 'SEU CS',
     url: 'https://je1ght.top',
     avatar: 'https://github.com/Je1ghtxyuN.png',
   },
+  {
+    id: '',
+    name: 'fugod',
+    description: 'ZJU Turing',
+    url: 'https://fufu142857.github.io/',
+    avatar: 'https://github.com/fufu142857.png',
+  },
+
 ]
